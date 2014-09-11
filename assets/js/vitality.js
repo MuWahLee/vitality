3cb98692a2b8e844b12d9a686a4a52e10ca91edc
MuWahLee <mlee2569@hotmail.com>
2014-09-12 00:52:08 +0800

    added services popups

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -99,3 +99,10 @@ $(window).on("load resize",function(e){
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+
+
+// core services popup //
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
