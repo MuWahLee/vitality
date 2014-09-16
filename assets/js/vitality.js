52a410f77fd10bbf1e0e4db07f25e68fecfa077c
MuWahLee <mlee2569@hotmail.com>
2014-09-16 22:08:05 +0800

    updated team images with colour photos

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -104,5 +104,18 @@ $('body').scrollspy({
 // core services popup //
 $('.open-popup-link').magnificPopup({
   type:'inline',
-  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  gallery: {
+    enabled: true
+  },
+});
+
+
+// team popup //
+$('.team-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  gallery: {
+    enabled: true
+  },
 });
